e2cb132d0156043567695b3bd98fd489ee1208bd
MnzJon <jonas_znm@outlook.com>
2023-04-06 20:52:33 +0100

    Environment setup reached

--- CmdArchive.py
+++ CmdArchive.py
@@ -4,7 +4,20 @@
 # TemplateFlags.json -> Stores flags pertaining to certain Options
 # RequiredFlags.json -> Stores the flags that are required, but need to be entered everytime.
 # TemplateCommands.json -> Stores commands that are useful for the user later on
+import os
 
 class CmdArchive():
     def __init__(self, directory="~/.local/share/cmd_archive/"):
-        self.home_directory = directory
+        self.home_directory = os.path.expanduser(directory)
+
+    def setup_environment(self):
+        if os.path.exists(self.home_directory) == False:
+            os.mkdir(self.home_directory)
+
+        return self
+
+    def environment_test(self):
+        if os.path.exists(self.home_directory):
+            print("Home Directory -> PASS")
+        else:
+            print("Home Directory not setup -> FAIL")
---
+++ test_cmdarchive.py
@@ -0,0 +1,5 @@
+from CmdArchive import CmdArchive 
+
+cmdArch = CmdArchive()
+cmdArch.setup_environment().environment_test()
+
