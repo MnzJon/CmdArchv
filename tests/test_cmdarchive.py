ac35c5dbcf6c9f45d848dbfc41d602eff735e106
MnzJon <jonas_znm@outlook.com>
2023-04-09 13:06:34 +0100

    Added test for Sessionstate

--- tests/test_cmdarchive.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("../")
-
-from CmdArchive import CmdArchive
-import os
-
-def test_cmdarchive():
-    cmdArch = CmdArchive("./test_home_archive/")
-    cmdArch.setup_environment()
--- tests/test_session_holder.py
+++ tests/test_session_holder.py
@@ -20,6 +20,25 @@ def test_session_stateholder():
     
     session_state.insert_to_recent(cmd)
     assert str(to_command(session_state.get_recent_cmd())) == str(cmd)
-    
 
+def test_history():
+    cmdBuilder = CommandBuilder()
+    cmdBuilder = cmdBuilder.set_script("ls").append_token_flag(["FLAG1","ON"]).set_build_cmd("build").set_epilogue("; notify.sh")
+    cmd = cmdBuilder.build()
+
+    session_state = SessionStateHolder("global", "./session_stateholder_test/")
+    session_state.setup_environment()
+
+    session_state.record_cmd(cmd)
+
+    # Test recent cmd
+    assert str(to_command(session_state.get_recent_cmd())) == str(cmd)
+
+    # Get the only command in history
+    # No way to specify the ID via time
+    old_cmd = {}
+    for k in session_state.get_history().keys():
+        old_cmd = session_state.get_history()[k]
+
+    assert str(to_command(old_cmd)) == str(cmd)
 
